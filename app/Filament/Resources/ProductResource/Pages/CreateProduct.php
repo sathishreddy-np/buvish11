5675b66168464dd07396f75d7cc4becd52d1a5ca
sathishreddy-np <sathishreddy.np@gmail.com>
2024-03-30 17:36:44 +0530

    product

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -4,7 +4,12 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use App\Models\Product;
+use App\Models\Variant;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CreateProduct extends CreateRecord
 {
@@ -15,5 +20,82 @@ class CreateProduct extends CreateRecord
         return [
             Product::backAction(),
         ];
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $new_data = array(
+            'team_id' => Filament::getTenant()->id,
+            'name' => $data['name'],
+            'brand_id' => $data['brand_id'],
+            'description' => $data['description'],
+            'meta_title' => $data['meta_title'],
+            'meta_description' => $data['meta_description'],
+            'hsn_code' => $data['hsn_code'],
+            'sku_code' => $data['sku_code'],
+            'barcode' => $data['barcode'],
+            'is_taxable' => $data['is_taxable'],
+            'is_vat_applied' => $data['is_vat_applied'],
+            'is_coupon_applicable' => $data['is_coupon_applicable'],
+            'is_digital' => $data['is_digital'],
+            'digital_product_file' => $data['digital_product_file'],
+        );
+
+        $product_id = Product::create($new_data)->id;
+        $data['product_id'] = $product_id;
+        return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        Log::info($data);
+        $variants = [];
+        $variantIndex = 1;
+
+        // Loop through each set of attributes
+        while (isset($data['attribute_' . $variantIndex . '_1'])) {
+            $variant = [];
+            $variant['product_id'] = $data['product_id'];
+
+            // Loop through attributes for this variant
+            $attributeIndex = 1;
+            while (isset($data['attribute_' . $variantIndex . '_' . $attributeIndex])) {
+                $attributeKey = 'attribute_' . $variantIndex . '_' . $attributeIndex;
+                $valueKey = 'value_' . $variantIndex . '_' . $attributeIndex;
+                $variant['variants'][$data[$attributeKey]] = $data[$valueKey];
+                $attributeIndex++;
+            }
+
+            // Add image and price for this variant
+            $variant['image'] = $data['image_' . $variantIndex];
+            $variant['price'] = $data['price_' . $variantIndex];
+
+            // Add this variant to the variants array
+            $variants[] = $variant;
+            $variantIndex++;
+        }
+
+        foreach ($variants as $variant) {
+            $variant_id = Variant::create(
+                [
+                    'team_id' => Filament::getTenant()->id,
+                    'product_id' => $variant['product_id'],
+                    'image' => $variant['image'],
+                    'price' => $variant['price']
+                ]
+            )->id;
+
+            foreach ($variant['variants'] as $attribute_id => $value_id) {
+                DB::table('attribute_variant')->insert([
+                    'variant_id' => $variant_id,
+                    'attribute_id' => $attribute_id,
+                    'attribute_value_id' => $value_id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+
+        return static::getModel()::find($variant_id);
     }
 }
