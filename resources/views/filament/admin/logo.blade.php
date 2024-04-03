805c63278e94c2923141384bcd9c71962246f878
sathishreddy-np <sathishreddy.np@gmail.com>
2024-04-03 10:32:22 +0530

    logo

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -1,4 +1,6 @@
 <div style="display: flex; align-items: center;">
+    @if(isset($image) && $image)
     <img src="{{ asset($image) }}" alt="{{ $name }}" style="height: 2rem;">
+    @endif
     <span style="margin-left: 5px;"><strong>{{ \Illuminate\Support\Str::words($name, 2, '') }}</strong></span>
 </div>
